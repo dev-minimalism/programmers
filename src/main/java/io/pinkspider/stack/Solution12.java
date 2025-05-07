06445407da8b20780ddadf131de746c3c263430c
Yungoo Park <ygpark@lendingmachine.co.kr>
2025-05-07 18:14:13 +0900

    250507

---
+++ src/main/java/io/pinkspider/stack/Solution12.java
@@ -0,0 +1,30 @@
+package io.pinkspider.stack;
+
+import java.util.Stack;
+
+// 짝지어 제거하기
+public class Solution12 {
+
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int i = 1; i < n; i++) {
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int j = stack.pop();
+                answer[j] = i - j;
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            int j = stack.pop();
+            answer[j] = n - j - 1;
+        }
+
+        return answer;
+    }
+}
