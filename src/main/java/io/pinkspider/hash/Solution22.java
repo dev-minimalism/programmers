f6a68527e849c8a4899ca06c5c7de859a71020ce
Yungoo Park <ygpark@lendingmachine.co.kr>
2025-06-23 15:56:20 +0900

    문제 22 베스트 앨범

---
+++ src/main/java/io/pinkspider/hash/Solution22.java
@@ -0,0 +1,46 @@
+package io.pinkspider.hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+// 베스트 앨범
+// O(NlogN)
+public class Solution22 {
+
+    public static int[] solution(String[] genres, int[] plays) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        HashMap<String, ArrayList<int[]>> genreMap = new HashMap<>();
+        HashMap<String, Integer> playMap = new HashMap<>();
+
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+
+            if (!genreMap.containsKey(genre)) {
+                genreMap.put(genre, new ArrayList<>());
+                playMap.put(genre, 0);
+            }
+            genreMap.get(genre).add(new int[]{i, play});
+            playMap.put(genre, playMap.get(genre) + play);
+        }
+
+        Stream<Map.Entry<String, Integer>> sortedGenre = playMap.entrySet().stream()
+            .sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
+
+        sortedGenre.forEach(entry -> {
+            Stream<int[]> sortedSongs = genreMap.get(entry.getKey()).stream()
+                .sorted((o1, o2) -> Integer.compare(o2[1], o1[1]))
+                .limit(2);
+
+            sortedSongs.forEach(song -> answer.add(song[0]));
+        });
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
