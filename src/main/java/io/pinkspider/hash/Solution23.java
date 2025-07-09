086996e2a50cbbe94d8d12ebac676598a80df088
Yungoo Park <ygpark@lendingmachine.co.kr>
2025-07-09 15:31:46 +0900

    문제 23 신고 결과 받기

---
+++ src/main/java/io/pinkspider/hash/Solution23.java
@@ -0,0 +1,42 @@
+package io.pinkspider.hash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+// 23. 신고 결과 받기
+public class Solution23 {
+
+    public static int[] solution(String[] id_list, String[] report, int k) {
+
+        int[] answer = new int[id_list.length];
+
+        HashMap<String, HashSet<String>> reportedUser = new HashMap<>();
+        HashMap<String, Integer> count = new HashMap<>();
+
+        for (String r : report) {
+            String[] s = r.split(" ");
+            String userId = s[0];
+            String reportedUserId = s[1];
+
+            if (!reportedUser.containsKey(reportedUserId)) {
+                reportedUser.put(reportedUserId, new HashSet<>());
+            }
+            reportedUser.get(reportedUserId).add(userId);
+        }
+
+        for (Map.Entry<String, HashSet<String>> entry : reportedUser.entrySet()) {
+            if (entry.getValue().size() == k) {
+                for (String userId : entry.getValue()) {
+                    count.put(userId, count.getOrDefault(userId, 0) + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = count.getOrDefault(id_list[i], 0);
+        }
+
+        return answer;
+    }
+}
